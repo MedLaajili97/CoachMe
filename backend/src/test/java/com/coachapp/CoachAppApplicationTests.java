e3206369652af353d56a5de67942db48846baabc
Mohamed Laajili <mohamed.laajili@b2m-it.com>
2026-04-25 01:31:28 +0100

    #3 - Bootstrap Spring Boot project with all required dependencies (pom.xml)

---
+++ backend/src/test/java/com/coachapp/CoachAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coachapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //empty for future tests
+    }
+}
